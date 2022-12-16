041bed7344ee70b321040e1d5b07354c610440ad
Marek Pędziński <118841334+MarekGameDev@users.noreply.github.com>
2022-12-16 22:38:03 +0100

    Armchair added

--- Source/SmartAssetsLibrary/Walker.cpp
+++ Source/SmartAssetsLibrary/Walker.cpp
@@ -9,6 +9,9 @@ AWalker::AWalker()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
+	RootComponent = Body;
+
 }
 
 // Called when the game starts or when spawned
